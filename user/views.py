d5fd3c7c0a7e488bef45ff542f3cd91fd6a80b1a
Illia Savinov <dresar2404@gmail.com>
2026-04-23 14:34:10 +0200

    add all required files to work with user authentication and registration

--- user/views.py
+++ user/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from drf_spectacular.utils import extend_schema
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView,
+                                            TokenVerifyView)
 
-# Create your views here.
+from user.serializers import UserSerializer
+
+
+@extend_schema(tags=["User Authentication & Profile"],
+               summary="requires unique login and password to register")
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny, )
+
+
+@extend_schema(tags=["User Authentication & Profile"])
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
+
+
+@extend_schema(tags=["User Auth"], summary="Obtain JWT token pair")
+class DecoratedTokenObtainPairView(TokenObtainPairView):
+    pass
+
+
+@extend_schema(tags=["User Auth"], summary="Refresh JWT token")
+class DecoratedTokenRefreshView(TokenRefreshView):
+    pass
+
+
+@extend_schema(tags=["User Auth"], summary="Verify JWT token")
+class DecoratedTokenVerifyView(TokenVerifyView):
+    pass
